ee7004588a64e318e87f6b86e84d60e02a9cc110
Elena Kravtsova <EGKravtsova@inno.tech>
2025-10-24 19:12:54 +0300

    box added

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,5 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
     }
 }
